3d8ddf869075a9177a3a974ec8194a40ce319ab6
Aliisright <alihasan.me@me.com>
2017-11-03 17:08:54 +0100

    ajout base de donnees

--- connection-db/connexion-bdd.php
+++ connection-db/connexion-bdd.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $tdl = new PDO('mysql:host=https://databases-auth.000webhost.com/;dbname=id908762_todolist;charset=utf8', 'id908762_alihasan', '123456', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $tdl = new PDO('mysql:host=localhost;dbname=todolist', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   } catch (Exception $e) {
     die('Erreur: ' . $e->getMessage());
   }
